9ce2e1bdcf99eeefedb64d70d2d210879eafb013
CFinck42 <chris.finck1@gmail.com>
2025-06-05 18:06:46 -0400

    updated py code

--- chrisBeta_analyze_sequences.py
+++ chrisBeta_analyze_sequences.py
@@ -4,16 +4,21 @@ seq1 = Seq("AGTACACTGGT")
 seq2 = Seq("AGTAAACTGGT")
 seq3 = Seq("AGTACACTAAT")
 seq4 = Seq("CGTACACTGGT")
-seq5 = Seq("AGTGGGGGGGT")
-sequences = ["seq1","seq2","seq3","seq4","seq5"]
+seq5 = Seq("AGTGGGGXGGT")
+seq_list = ["seq1","seq2","seq3","seq4","seq5"]
 
-def gc_content:
+def gc_content(seq):
+    if len(seq) == 0:
+        return 0.0  # Handle empty sequences to avoid division by zero  
+    else:          
+        return ((seq.count("G") + seq.count("C")) / len(seq)) * 100
 
-    gc_count = sequence.count('G') + sequence.count('C')
-        total_bases = len(sequence)
+def is_valid(seq):
+    return all(base in "ATCG" for base in seq)
 
-        if total_bases == 0:
-            return 0.0  # Handle empty sequences to avoid division by zero
-
-    gc_percentage = (gc_count / total_bases) * 100
-    return gc_percentage
+for seq in seq_list:
+    print(f"Sequence: {seq}")
+    print(f"Length: {len(seq)} nt")
+    print(f"GC Content: {gc_content(seq):.2%}")
+    print(f"Valid DNA Sequence?: {is_valid(seq)}")
+    print(f"Reverse Complement: {Seq(seq).reverse_complement()}")
